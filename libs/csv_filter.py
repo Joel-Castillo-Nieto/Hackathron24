536179101652f33a1fe05b0ad3ba280836b69ec8
NIU1745916 <Gerard.PuigO@autonoma.cat>
2024-10-27 05:27:51 +0100

    Modificat el nom de la llibreria

--- libs/csv_filter.py
+++ libs/csv_filter.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-def full_csv(route_to_notes_csv, delim=","):
-    csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
-    return csv_data
-
-def csv_aula_F(route_to_notes_csv, aula, delim=","):
+# Get info from notes.csv, filter it with id_aula, then return the data.
+def aula_F(route_to_notes_csv, aula, delim=","):
     csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
 
     csv_data = csv_data[csv_data["F_Grade"].notna()]
@@ -12,44 +9,32 @@ def csv_aula_F(route_to_notes_csv, aula, delim=","):
 
     return csv_clean
 
-def csv_partial_F(route_to_notes_csv, delim=","):
-    csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
-
-    csv_data = csv_data[csv_data["F_Grade"].notna()]
-    csv_clean = csv_data[csv_data["P_Grade"].notna()]
+# Pass the data filtered (for example with aula_F()) then returns it with the
+# students that did the Partial Exam and Final Exam.
+def partial_F(pd_data):
+    csv_clean = pd_data[pd_data["F_Grade"].notna()]
+    csv_clean = csv_clean[csv_clean["P_Grade"].notna()]
 
     return csv_clean
 
-def csv_no_partial_F(route_to_notes_csv, delim=","):
-    csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
-
-    csv_data = csv_data[csv_data["F_Grade"].notna()]
+# The same as partial_F but with students that didn't do the partial.
+def no_partial_F(pd_data):
+    csv_data = pd_data[pd_data["F_Grade"].notna()]
     csv_clean = csv_data[csv_data["P_Grade"].notna() == False]
 
     return csv_clean
 
-def csv_partial_R(route_to_notes_csv, delim=","):
-    csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
-
-    csv_data = csv_data[csv_data["F_Grade"].notna()]
+# Pass the data filtered (for example with aula_F()) then r
+def partial_R(pd_data):
+    csv_data = pd_data[pd_data["F_Grade"].notna()]
     csv_data = csv_data[csv_data["P_Grade"].notna()]
     csv_clean = csv_data[csv_data["R_Grade"].notna()]
 
     return csv_clean
 
-def csv_no_partial_R(route_to_notes_csv, delim=","):
-    csv_data = pd.read_csv(route_to_notes_csv, delimiter=delim)
-
-    csv_data = csv_data[csv_data["F_Grade"].notna()]
+def no_partial_R(pd_data):
+    csv_data = pd_data[pd_data["F_Grade"].notna()]
     csv_data = csv_data[csv_data["P_Grade"].notna() == False]
     csv_clean = csv_data[csv_data["R_Grade"].notna()]
 
     return csv_clean
-
-
-
-
-
-
-
-csv_no_partial_R("../datos/notes.csv",";")
